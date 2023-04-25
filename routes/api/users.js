03b2823f4038a6c22ab4ddfd4618df31abecdeca
Otmane Aatik <aaotmane@gmail.com>
2023-04-25 13:46:27 -0400

    Authorization middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const usersCtrl = require('../../controllers/api/users')
+const usersCtrl = require('../../controllers/api/users');
+// require the authorization middleware function
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 //POST /api/users
 router.post('/', usersCtrl.create)
 //POST /api/users/login
 router.post("/login", usersCtrl.login);
 
-router.get("/check-token", usersCtrl.checkToken);
+router.get("/check-token", ensureLoggedIn, usersCtrl.checkToken);
 
 module.exports = router;
